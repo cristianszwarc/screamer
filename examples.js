45ca1bef9c7c00c3bed125c6dfa94e83671a9d45
cristianszwarc <cristianszwarc@gmail.com>
2020-09-19 01:00:39 +1200

    examples

--- examples.js
+++ examples.js
@@ -4,6 +4,7 @@ scream('Bob', 'Name')
 scream(45, 'Age')
 scream([3,4,5,6], 'Numbers')
 scream({ name: 'Bob', age: 45}, 'Object')
+screamify({ name: 'Bob', age: 45}, 'JSON.stringify')
 
 const circularReference = {
   works: 'yeah!'
